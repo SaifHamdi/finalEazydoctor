837a99b546cd55c8c16fc4f9cf5e0de3c10184eb
merna <mernaashraf910@gmail.com>
2022-04-19 22:02:43 +0200

    test

--- lib/Register.dart
+++ lib/Register.dart
@@ -10,98 +10,100 @@ class RegisterScreen extends StatelessWidget{
       children: [
         Scaffold(
           body:
-          Container(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(25, 100, 25, 25),
-              child: Form(key: formKey,
-                child: Column(crossAxisAlignment: CrossAxisAlignment.center,mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(child: Image.asset('assets/images/x1.0/register.jpg',width: 175,height: 190,),),
-                    Container(
-                      child: TextFormField(
-                        decoration: InputDecoration(labelText: 'first name'),
-                        onChanged: (text){
-                          firstName=text;
+          SingleChildScrollView(
+            child: Container(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(25, 100, 25, 25),
+                child: Form(key: formKey,
+                  child: Column(crossAxisAlignment: CrossAxisAlignment.center,mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(child: Image.asset('assets/images/x1.0/register.jpg',width: 175,height: 190,),),
+                      Container(
+                        child: TextFormField(
+                          decoration: InputDecoration(labelText: 'first name'),
+                          onChanged: (text){
+                            firstName=text;
+                          },
+                          validator: (text){
+                            if(text==null|| text.trim().isEmpty){
+                              return 'please enter first name';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      Container(
+                        child: TextFormField(
+                            decoration: InputDecoration(labelText: 'last name'), onChanged: (text){
+                          lastName=text;
                         },
-                        validator: (text){
-                          if(text==null|| text.trim().isEmpty){
-                            return 'please enter first name';
-                          }
-                          return null;
-                        },
-                      ),
-                    ),
-                    Container(
-                      child: TextFormField(
-                          decoration: InputDecoration(labelText: 'last name'), onChanged: (text){
-                        lastName=text;
-                      },
-                          validator: (text){
-                            if(text==null|| text.trim().isEmpty){
-                              return 'please enter last name';
+                            validator: (text){
+                              if(text==null|| text.trim().isEmpty){
+                                return 'please enter last name';
+                              }
+                              return null;
                             }
-                            return null;
-                          }
+                        ),
                       ),
-                    ),
-                    Container(
-                      child: TextFormField(
-                          decoration: InputDecoration(labelText: ' username'),
-                          onChanged: (text){
-                            userName=text;
-                          },
-                          validator: (text){
-                            if(text==null|| text.trim().isEmpty){
-                              return 'please enter user name';
+                      Container(
+                        child: TextFormField(
+                            decoration: InputDecoration(labelText: ' username'),
+                            onChanged: (text){
+                              userName=text;
+                            },
+                            validator: (text){
+                              if(text==null|| text.trim().isEmpty){
+                                return 'please enter user name';
+                              }
+                              return null;
                             }
-                            return null;
-                          }
+                        ),
                       ),
-                    ),
-                    Container(
-                      key: formKey,
-                      child: TextFormField(
-                          decoration: InputDecoration(labelText: 'Email'),
-                          onChanged: (text){
-                            email=text;
-                          },
-                          validator: (text){
-                            if(text==null|| text.trim().isEmpty){
-                              return 'please enter your email';
+                      Container(
+                        key: formKey,
+                        child: TextFormField(
+                            decoration: InputDecoration(labelText: 'Email'),
+                            onChanged: (text){
+                              email=text;
+                            },
+                            validator: (text){
+                              if(text==null|| text.trim().isEmpty){
+                                return 'please enter your email';
+                              }
+                              if(!isValidEmail(email)){
+                                return 'please enter a valid email';
+                              }
+                              return null;
                             }
-                            if(!isValidEmail(email)){
-                              return 'please enter a valid email';
+                        ),
+                      ),
+                      Container(
+                        child: TextFormField(
+                            decoration: InputDecoration(labelText: 'password'),
+                            onChanged: (text){
+                              password=text;
+                            },
+                            validator: (text){
+                              if(text==null|| text.trim().isEmpty){
+                                return 'please enter your password';
+                              }if(text.length<6){
+                                return 'password should be at least 6 characters';
+                              }
+                              return null;
                             }
-                            return null;
-                          }
+                        ),
                       ),
-                    ),
-                    Container(
-                      child: TextFormField(
-                          decoration: InputDecoration(labelText: 'password'),
-                          onChanged: (text){
-                            password=text;
-                          },
-                          validator: (text){
-                            if(text==null|| text.trim().isEmpty){
-                              return 'please enter your password';
-                            }if(text.length<6){
-                              return 'password should be at least 6 characters';
+                      Container(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            if(formKey.currentState?.validate()==true){
+                              createAccountWithFirebaseAuth();
                             }
-                            return null;
-                          }
-                      ),
-                    ),
-                    Container(
-                      child: ElevatedButton(
-                        onPressed: () {
-                          if(formKey.currentState?.validate()==true){
-                            createAccountWithFirebaseAuth();
-                          }
-                        },child: Text('create account'),
-                      ),
-                    )
-                  ],
+                          },child: Text('create account'),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
